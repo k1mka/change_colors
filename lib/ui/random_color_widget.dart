6d099939eb645d9d39418762ca20fd7b492b0637
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-06-28 17:42:03 +0300

    added random color and updated background color

--- lib/ui/random_color_widget.dart
+++ lib/ui/random_color_widget.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class RandomColorWidget extends StatelessWidget {
+class RandomColorWidget extends StatefulWidget {
   const RandomColorWidget({Key? key}) : super(key: key);
 
   @override
+  State<RandomColorWidget> createState() => _RandomColorWidgetState();
+}
+
+class _RandomColorWidgetState extends State<RandomColorWidget> {
+  Color backgroundColor = Colors.green;
+
+  void updateColor() {
+    Random random = Random();
+    final randomNumber = random.nextInt(0xffffffff);
+    Color randomColor = Color(randomNumber);
+    setState(() => backgroundColor = randomColor);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 500.0,
-      height: 1000.0,
-      color: Colors.red,
+    return GestureDetector(
+      onTap: updateColor,
+      child: Container(
+        width: 500.0,
+        height: 1000.0,
+        color: backgroundColor,
+      ),
     );
   }
 }
