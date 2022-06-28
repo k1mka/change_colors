f9b535fb4c0a4880a1cb72a367ed7e498bb6148f
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-06-28 22:02:12 +0300

    made text random color on pressed

--- lib/ui/hey_text.dart
+++ lib/ui/hey_text.dart
@@ -1,14 +1,31 @@
+import 'package:change_colors/tools/color_generator.dart';
 import 'package:flutter/material.dart';
 
-class HeyText extends StatelessWidget {
+class HeyText extends StatefulWidget {
   const HeyText({Key? key}) : super(key: key);
   static const text = 'Hey there';
 
   @override
+  State<HeyText> createState() => _HeyTextState();
+}
+
+class _HeyTextState extends State<HeyText> with ColorGenerator {
+  Color textColor = Colors.black;
+
+  void updateTextColor() {
+    setState(() {
+      textColor = generateColor();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text(
-      text,
-      style: TextStyle(fontSize: 68.0),
+    return GestureDetector(
+      onTap: updateTextColor,
+      child: Text(
+        HeyText.text,
+        style: TextStyle(fontSize: 68.0, color: textColor),
+      ),
     );
   }
 }
